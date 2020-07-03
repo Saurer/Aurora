e173783ef64374a6e916836ff6f2822dc10743a1
Saurer <redmmond@gmail.com>
2020-07-03 09:45:10 +0900

    Remove default value from attr

--- Aurora/Controllers/Attr.cs
+++ Aurora/Controllers/Attr.cs
@@ -2,12 +2,10 @@ namespace Aurora.Controllers {
     public sealed class Attr {
         public int ID { get; private set; }
         public string Name { get; private set; }
-        public string DefaultValue { get; private set; }
 
-        public Attr(int id, string name, string defaultValue = null) {
+        public Attr(int id, string name) {
             ID = id;
             Name = name;
-            DefaultValue = defaultValue;
         }
     }
 }
