188e28110b78ac5e094e9b017f885f05c4ecbcd9
Saurer <redmmond@gmail.com>
2020-07-05 05:11:37 +0900

    Add public state accessor to the engine

--- AuroraCore/EngineBase.cs
+++ AuroraCore/EngineBase.cs
@@ -7,8 +7,9 @@ using AuroraCore.Types;
 namespace AuroraCore {
     public class EngineBase {
         private List<Controller> controllers = new List<Controller>();
-        private TransientState state = new TransientState();
         private Dictionary<int, List<Action<IEventData>>> reactions = new Dictionary<int, List<Action<IEventData>>>();
+
+        public TransientState State { get; private set; } = new TransientState();
 
         public void AddReaction(int eventID, Action<IEventData> reaction) {
             if (!reactions.ContainsKey(eventID)) {
@@ -22,7 +23,7 @@ namespace AuroraCore {
             int execCount = 0;
 
             foreach (var reaction in reactions) {
-                if (state.IsEventAncestor(reaction.Key, e.ValueID)) {
+                if (State.IsEventAncestor(reaction.Key, e.ValueID)) {
                     foreach (var item in reaction.Value) {
                         execCount++;
                         yield return item;
@@ -38,7 +39,7 @@ namespace AuroraCore {
         }
 
         public void AddController<T>() where T : Controller, new() {
-            Controller controller = Controller.Instantiate<T>(state);
+            Controller controller = Controller.Instantiate<T>(State);
             IEnumerable<ReactionBase> reactions = controller.GetReactions();
 
             foreach (var reaction in reactions) {
@@ -52,7 +53,7 @@ namespace AuroraCore {
         }
 
         public void AddType<T>(string name) where T : DataType {
-            state.Types.Register<T>(name);
+            State.Types.Register<T>(name);
         }
 
         public void ProcessEvent(IEventData e) {
