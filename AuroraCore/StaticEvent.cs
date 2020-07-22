14e20036c1f131b1c6ffacdb7d5a679cefb371ca
Saurer <redmmond@gmail.com>
2020-07-22 22:46:58 +0900

    Add role static events

--- AuroraCore/StaticEvent.cs
+++ AuroraCore/StaticEvent.cs
@@ -3,7 +3,9 @@ namespace AuroraCore {
         public const int Event = 0;
         public const int SubEvent = 1;
         public const int Actor = 2;
+        public const int ActorModel = 20;
         public const int Entity = 3;
+        public const int EntityModel = 10;
         public const int Relation = 4;
         public const int Attribute = 5;
         public const int AttributeProperty = 6;
@@ -11,5 +13,7 @@ namespace AuroraCore {
         public const int Model = 7;
         public const int Individual = 8;
         public const int DataType = 12;
+        public const int Role = 23;
+        public const int RoleModel = 24;
     }
 }
