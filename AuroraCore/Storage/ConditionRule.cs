43ac43b1ffda4572cf7190373aa4c61f223f375e
Frank Horrigan <redmmond@gmail.com>
2020-10-04 10:37:48 +0300

    Modify condition rule to allow serialization

--- AuroraCore/Storage/ConditionRule.cs
+++ AuroraCore/Storage/ConditionRule.cs
@@ -6,26 +6,32 @@ namespace AuroraCore.Storage {
         public class EventConditionRule : ConditionRule {
             public int EventID { get; private set; }
 
+            public EventConditionRule() {
+
+            }
+
             public EventConditionRule(int eventID) {
                 EventID = eventID;
             }
         }
 
         public abstract class ComplexConditionRule : ConditionRule {
-            public IEnumerable<ConditionRule> Values { get; private set; }
+            public List<ConditionRule> Values { get; private set; }
 
             public ComplexConditionRule(IEnumerable<ConditionRule> values) {
-                Values = values;
+                Values = new List<ConditionRule>(values);
             }
         }
 
         public class ConjunctionRule : ComplexConditionRule {
+            public ConjunctionRule() : base(Array.Empty<ConditionRule>()) { }
             public ConjunctionRule(IEnumerable<ConditionRule> values) : base(values) {
 
             }
         }
 
         public class DisjunctionRule : ComplexConditionRule {
+            public DisjunctionRule() : base(Array.Empty<ConditionRule>()) { }
             public DisjunctionRule(IEnumerable<ConditionRule> values) : base(values) {
 
             }
@@ -42,12 +48,14 @@ namespace AuroraCore.Storage {
         }
 
         public class PropertyEqualityRule : PropertyValueRule {
+            public PropertyEqualityRule() : base(default(int), null) { }
             public PropertyEqualityRule(int propertyID, string value) : base(propertyID, value) {
 
             }
         }
 
         public class PropertyInequalityRule : PropertyValueRule {
+            public PropertyInequalityRule() : base(default(int), null) { }
             public PropertyInequalityRule(int propertyID, string value) : base(propertyID, value) {
 
             }
